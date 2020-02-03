628546adaf0c426c2517827bf883559ab85fd983
lzcGlb <meetcwx20191020@163.com>
2020-02-03 13:02:39 +0800

    第一次修改

---
+++ src/main/java/com/lzcong/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.lzcong.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/info")
+    public String info(){
+        return "hello spring boot";
+    }
+}
